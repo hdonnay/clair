2c0e48e1fadb1b5723cd29861e9912c14085c357
Hank Donnay <hdonnay@redhat.com>
2021-02-19 11:38:32 -0600

    all: switch to using codec package
    This change switches almost all usages of "encoding/json" over to using
    encoders/decoders provided by the codec package.
    
    Remaining usages are in tests, scripts, or when the interface requires
    use of a byte slice directly.
    
    The following command was used to hunt down usages:
    
        git grep encoding/json -- ':^*_test.go'
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- notifier/callback.go
+++ notifier/callback.go
@@ -1,6 +1,7 @@
 package notifier
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/url"
@@ -12,40 +13,39 @@ import (
 // and receive notifications.
 type Callback struct {
 	NotificationID uuid.UUID `json:"notification_id"`
-	Callback       url.URL   `json:"callback"`
+	Callback       url.URL   `json:"callback,string"`
 }
 
+// CbMessage is the actual type used for over the wire. The above's struct tags
+// are for documentation only.
+type cbMessage struct {
+	ID       *uuid.UUID `json:"notification_id"`
+	Callback string     `json:"callback"`
+}
+
+// MarshalJSON implements json.Marshaler.
 func (cb Callback) MarshalJSON() ([]byte, error) {
-	var m = map[string]string{
-		"notification_id": cb.NotificationID.String(),
-		"callback":        cb.Callback.String(),
-	}
-	return json.Marshal(m)
+	// This writes out a block of bytes directly, instead of allocating a
+	// cbMessage structure, serializing the URL, then copying it into a []byte.
+	b := bytes.NewBufferString(`{"callback":"`)
+	b.WriteString(cb.Callback.String())
+	b.WriteString(`","notification_id":"`)
+	b.WriteString(cb.NotificationID.String())
+	b.WriteString(`"}`)
+	return b.Bytes(), nil
 }
 
+// UnmarshalJSON implements json.Unmarshaler.
 func (cb *Callback) UnmarshalJSON(b []byte) error {
-	var m = make(map[string]string, 2)
-	err := json.Unmarshal(b, &m)
-	if err != nil {
+	var msg cbMessage
+	msg.ID = &cb.NotificationID
+	if err := json.Unmarshal(b, &msg); err != nil {
 		return err
 	}
-	if _, ok := m["notification_id"]; !ok {
-		return fmt.Errorf("json unmarshal failed. webhook requires a \"notification_id\" field")
-	}
-	if _, ok := m["callback"]; !ok {
-		return fmt.Errorf("json unmarshal failed. webhook requires a \"callback\" field")
-	}
-
-	uid, err := uuid.Parse(m["notification_id"])
-	if err != nil {
-		return fmt.Errorf("json unmarshal failed. malformed notification uuid: %v", err)
-	}
-	cbURL, err := url.Parse(m["callback"])
+	u, err := url.Parse(msg.Callback)
 	if err != nil {
 		return fmt.Errorf("json unmarshal failed. malformed callback url: %v", err)
 	}
-
-	(*cb).NotificationID = uid
-	(*cb).Callback = *cbURL
+	cb.Callback = *u
 	return nil
 }
